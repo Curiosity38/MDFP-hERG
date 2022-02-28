c0b2f2dffb2c85a0d2a75555caebd7174af26943
Curiosity38 <100185874+Curiosity38@users.noreply.github.com>
2022-02-28 21:56:26 +0800

    Update feature_importance.r

--- MDFP-hERG/feature_importance.r
+++ MDFP-hERG/feature_importance.r
@@ -1,10 +1,7 @@
-setwd("/home/zl/zl/MDFP-hERG-3")
-
 library(tidyverse)
 library(randomForest)
 
 IC50 <- read.csv("IC50_all.csv", stringsAsFactors  = FALSE, header = TRUE)
-# pIC50 <- select(IC50,-IC50)
 pIC50 <- IC50
 
 MDFP <- read.csv("MDFP_4_7836.csv", stringsAsFactors  = FALSE, header = TRUE)
@@ -30,7 +27,7 @@ ECFP4_train <- filter(ECFP4, Training.Test == "Training") %>% select(-Name, -Tra
 MDFP3D11_train <- filter(MDFP3D11, Training.Test == "Training") %>% select(-Name, -Training.Test)
 FP3DFP_train <- filter(FP3DFP, Training.Test == "Training") %>% select(-Name, -Training.Test)
 
-#选择RFE选出的特征
+#Choose features seleted by RFE
 MDFP_rfe_predictors <- read.csv(file = "MDFP_rfe_predictors.csv", row.names = 1)
 PropertyFP_rfe_predictors <- read.csv(file = "PropertyFP_rfe_predictors.csv", row.names = 1)
 BaselineFP_rfe_predictors <- read.csv(file = "BaselineFP_rfe_predictors.csv", row.names = 1)
@@ -45,7 +42,7 @@ ECFP4_train <- select(ECFP4_train, pIC50, all_of(as.character(ECFP4_rfe_predicto
 MDFP3D11_train <- select(MDFP3D11_train, pIC50, all_of(as.character(MDFP3D11_rfe_predictors$x)))
 FP3DFP_train <- select(FP3DFP_train, pIC50, all_of(as.character(FP3DFP_rfe_predictors$x)))
 
-#使用随机森林计算选出特征的重要性
+#Use Random Forest to calculate the importance of the selected features
 MDFP_rf <- randomForest(pIC50 ~ ., data = MDFP_train, importance = TRUE, ntree=500)
 PropertyFP_rf <- randomForest(pIC50 ~ ., data = PropertyFP_train, importance = TRUE)
 BaselineFP_rf <- randomForest(pIC50 ~ ., data = BaselineFP_train, importance = TRUE)
@@ -53,7 +50,7 @@ ECFP4_rf <- randomForest(pIC50 ~ ., data = ECFP4_train, importance = TRUE)
 MDFP3D11_rf <- randomForest(pIC50 ~ ., data = MDFP3D11_train, importance = TRUE)
 FP3DFP_rf <- randomForest(pIC50 ~ ., data = FP3DFP_train, importance = TRUE)
 
-#输出重要性结果
+#Ptint the results of importance 
 write.csv(importance(MDFP_rf, scale=TRUE) , file = "MDFP_feature_importance.csv")
 write.csv(importance(PropertyFP_rf, scale=TRUE) , file = "PropertyFP_feature_importance.csv")
 write.csv(importance(BaselineFP_rf, scale=TRUE), file = "BaselineFP_feature_importance.csv")
@@ -62,7 +59,7 @@ write.csv(importance(MDFP3D11_rf, scale=TRUE) , file = "MDFP3D11_feature_importa
 write.csv(importance(FP3DFP_rf, scale=TRUE) , file = "FP3DFP_feature_importance.csv")
 
 
-#输出重要性plot结果
+#Output significance plot results
 pdf(file = "MDFP_feature_importance.pdf")
 varImpPlot(MDFP_rf)
 dev.off()
